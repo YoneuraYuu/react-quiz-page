94e900ee1d43ef3f0e7d9d45db98b5e1e43501d0
yoneura <juzeppemeattula@gmail.com>
2025-03-15 13:17:33 +0900

    [add]入力値を判定してResultPageに飛ばす

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import QuizPage from "../components/QuizPage";
 import { ErrorPage } from "../components/ErrorPage";
+import ResultPage from "../components/ResultPage";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<QuizPage />} />
+        <Route path="/result" element={<ResultPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
