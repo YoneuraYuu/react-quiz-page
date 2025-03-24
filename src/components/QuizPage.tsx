b3c344a450af231aa964d177ff54f8cec822270b
yoneura <juzeppemeattula@gmail.com>
2025-03-24 22:54:34 +0900

    feat:見た目の情報を追加、文字列がランダムで表示されるように追加

--- src/components/QuizPage.tsx
+++ src/components/QuizPage.tsx
@@ -1,7 +1,36 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Box, Card, CardContent, Button, Stack, styled } from "@mui/material";
 import useQuizStore from "../store/quizStore";
 import quizList from "../const/quiz";
+
+const StyledCard = styled(Card)`
+  background-color: ${(props) => props.theme.palette.primary.main};
+  color: ${(props) => props.theme.palette.primary.contrastText};
+`;
+
+const AnswerCard = styled(Card)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  background-color: ${(props) => props.theme.palette.primary.contrastText};
+  color: ${(props) => props.theme.palette.primary.main};
+  font-weight: bold;
+`;
+
+const CenteredBox = styled(Box)`
+  text-align: center;
+  margin: ${(props) => props.theme.spacing(2)};
+`;
+
+const CenteredCardContent = styled(CardContent)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding-top: 25px;
+`;
 
 const QuizPage = () => {
   const { quiz, userAnswer, setQuiz, setUserAnswer } = useQuizStore();
@@ -42,30 +71,92 @@ const QuizPage = () => {
     return array;
   };
 
+  // ランダムなひらがなを生成する関数
+  const getRandomHiragana = () => {
+    const hiragana =
+      "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+    return hiragana.charAt(Math.floor(Math.random() * hiragana.length));
+  };
+
+  // ランダムなカタカナを生成する関数
+  const getRandomKatakana = () => {
+    const katakana =
+      "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン";
+    return katakana.charAt(Math.floor(Math.random() * katakana.length));
+  };
+
   // ボタンを生成
   const createButton = () => {
     if (!quiz) return;
-    //選択肢から先頭1文字ずつ取り出して配列を作る
-    const choicesArray = [quiz.correct_answer, ...quiz.incorrect_answers].map(
-      (v) => v.charAt(count)
-    );
+
+    //選択肢から先頭1文字ずつ取り出して一時配列を作る
+    const tempChoicesArray = [
+      quiz.correct_answer,
+      ...quiz.incorrect_answers,
+    ].map((v) => v.charAt(count));
+
+    //一時配列から重複を削除したSetを作成し、足りない分をランダムな文字で埋める
+    let choicesArraySet = new Set(tempChoicesArray);
+    while (choicesArraySet.size !== tempChoicesArray.length) {
+      let char = /[ァ-ヴー・]/.test(tempChoicesArray[0])
+        ? getRandomKatakana()
+        : getRandomHiragana();
+      choicesArraySet.add(char);
+    }
+
     console.log(count);
     //選択肢をシャッフルしてボタンを生成
-    return shuffleArray(choicesArray).map((v, index) => {
+    return shuffleArray(Array.from(choicesArraySet)).map((v, index) => {
       return (
-        <button key={index} onClick={handleClick}>
+        <Button
+          key={index}
+          onClick={handleClick}
+          variant="contained"
+          sx={{
+            width: "100px",
+            height: "100px",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
           {v}
-        </button>
+        </Button>
       );
     });
   };
 
   return (
     <div>
-      <h1>問題</h1>
-      <p>{quiz?.question}</p>
-      <p>答え：{userAnswer}</p>
-      <div className="button">{createButton()}</div>
+      <CenteredBox>
+        <CenteredBox>
+          <StyledCard className="questionCard" variant="outlined">
+            <CardContent>
+              <h1>問題</h1>
+              <p>{quiz?.question}</p>
+            </CardContent>
+          </StyledCard>
+        </CenteredBox>
+        <CenteredBox>
+          <AnswerCard className="answerCard" variant="outlined">
+            <CenteredCardContent>
+              <p>答え：{userAnswer}</p>
+            </CenteredCardContent>
+          </AnswerCard>
+        </CenteredBox>
+        <CenteredBox
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mx: 2,
+            my: 2,
+          }}
+        >
+          <Stack direction="row" spacing={2}>
+            {createButton()}
+          </Stack>
+        </CenteredBox>
+      </CenteredBox>
     </div>
   );
 };
--- src/components/ResultPage.tsx
+++ src/components/ResultPage.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect } from "react";
 import useQuizStore from "../store/quizStore";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Card, styled, Typography, useTheme } from "@mui/material";
+
+const StyledBox = styled(Box)`
+  text-align: center;
+  margin: ${(props) => props.theme.spacing(2)};
+`;
 
 const ResultPage = () => {
   const { quiz, userAnswer, setUserAnswer } = useQuizStore();
   const navigate = useNavigate();
+  const theme = useTheme();
 
   useEffect(() => {
     if (!quiz) navigate("/");
@@ -20,13 +27,32 @@ const ResultPage = () => {
 
   return (
     <div>
-      <h1>{result}</h1>
-      <br />
-      <p>答え：{quiz?.correct_answer}</p>
-      <p>{quiz?.description}</p>
-      <p>あなたの回答：{userAnswer}</p>
-      <br />
-      <button onClick={handleClick}>次の問題へ</button>
+      <StyledBox>
+        <StyledBox>
+          <Card
+            variant="outlined"
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.primary.contrastText,
+            }}
+          >
+            <h1>{result}</h1>
+            <br />
+            <Typography variant="body1">
+              <strong>答え：{quiz?.correct_answer}</strong>
+            </Typography>
+            <Typography variant="body1">{quiz?.description}</Typography>
+            <Typography variant="body1" sx={{ my: 2 }}>
+              <strong>あなたの回答：{userAnswer}</strong>
+            </Typography>
+          </Card>
+        </StyledBox>
+        <StyledBox>
+          <Button variant="contained" onClick={handleClick}>
+            次の問題へ
+          </Button>
+        </StyledBox>
+      </StyledBox>
     </div>
   );
 };
